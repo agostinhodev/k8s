5cd5eca85296ed53f5533c34f1c13d2e9c27b282
agostinhodev <agostinho.developer@gmail.com>
2023-01-28 17:23:47 -0300

    feat: :sparkles: Updated packages

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 import http from "http";
+import os from "os";
+const PORT = 3000;
 
 http
   .createServer(function (req, res) {
-    const data = req.headers;
+    const hostName = os.hostname();
+
+    const data = {
+      hostName,
+      date: new Date().toString(),
+      host: req.headers.host,
+    };
 
     res.setHeader("Content-Type", "application/json");
     res.writeHead(200);
     res.end(JSON.stringify(data, null, 3));
   })
-  .listen(80, function () {
-    console.log("server start at port 80"); //the server object listens on port 80
+  .listen(PORT, function () {
+    console.log(`server start at port ${PORT}`);
   });
